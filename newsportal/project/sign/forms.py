e29ccc783083302692403fda92634b167e79aa9e
unknown <valerijmanukyan.manukyan@yandex.ru>
2022-12-02 19:36:42 +0300

    add new file

--- newsportal/project/sign/forms.py
+++ newsportal/project/sign/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.models import User
+from .models import User
 from django.contrib.auth.forms import UserCreationForm
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
