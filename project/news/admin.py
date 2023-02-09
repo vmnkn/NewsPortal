d2ef8252d8e05217d856ae45f9e5e2bc37480aeb
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-02-09 15:54:57 +0300

    extends admin panel

--- project/news/admin.py
+++ project/news/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Post, Category
 
 
-admin.site.register(Post)
+def restart_rating(modeladmin, request, queryset):
+    queryset.update(post_rating=0)
+
+
+restart_rating.short_description = 'Restart rating'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'data', 'rating', 'author')
+    list_filter = ('post_rating', 'data')
+    search_fields = ('title', 'category__name')
+    actions = [restart_rating]
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
