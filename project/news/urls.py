fcbd6e237f34665880ec3545b309375b01488eff
unknown <valerijmanukyan.manukyan@yandex.ru>
2023-03-07 21:30:01 +0300

    deleted caching

--- project/news/urls.py
+++ project/news/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import PostList, PostDetail, PostSearch, PostCreate, PostUpdate, PostDelete, CategoryList, subscribe
-from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
-    path('', cache_page(60)(PostList.as_view()), name='post_list'),
-    path('<int:pk>', cache_page(300)(PostDetail.as_view()), name='post_detail'),
+    path('', PostList.as_view(), name='post_list'),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
     path('search/', PostSearch.as_view(), name='post_search'),
     path('create/', PostCreate.as_view(), name='news_create'),
     path('<int:pk>/edit/', PostUpdate.as_view(), name='news_update'),
